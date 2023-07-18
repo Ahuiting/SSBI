ef759f4c7a7711832d4870b86a815d1aa53bdf7f
Ahuiting <huiting.xu@student.uni-tuebingen.de>
2023-07-18 22:07:52 +0200

    A10 added

---
+++ Assignment10/README.md
@@ -0,0 +1,8 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+from scipy.optimize import curve_fit
+
+
+try 'python3 Renschler-Sperl_Xu_Assignment10.py -i human_interactome.csv'
--- Assignment10/Renschler-Sperl_Xu_Assignment10.py
+++ Assignment10/Renschler-Sperl_Xu_Assignment10.py
@@ -1,12 +1,11 @@
 # Author: Huiting Xu and Desirée Renschler-Sperl
-import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
 import argparse
+from scipy.optimize import curve_fit
 
 
-# create perse
 def create_parser():
     p = argparse.ArgumentParser(description=__doc__,
                                 formatter_class=argparse.RawDescriptionHelpFormatter)
@@ -23,11 +22,8 @@ def parse_csv(filename):
     return graph
 
 
-def main():
-    # df = pd.read_csv('test.csv',header=None)
-    graph = parse_csv('human_interactome.csv')
-    # graph = nx.Graph()
-    # graph.add_edges_from(df.values)
+def main(filename):
+    graph = parse_csv(filename)
 
     num_nodes = graph.number_of_nodes()
     print("Number of nodes:", num_nodes)
@@ -36,22 +32,44 @@ def main():
     print("Number of edges:", num_edges)
 
     degree = sorted([d for n, d in graph.degree()], reverse=True)
-    degree_count = np.bincount(degree)
+    degrees, degree_count = np.unique(degree, return_counts=True)
 
-    plt.loglog(degree_count, 'o')
+    log_degree=np.log10(degrees)
+    log_prob=np.log10(degree_count / num_nodes)
+
+    plt.loglog(degrees, degree_count / num_nodes, 'o')
     plt.xlabel('k (degree)')
     plt.ylabel('P(k)')
     plt.title('Node Degree Distribution')
+    plt.savefig('nodedist.pdf')
     plt.show()
 
+    def func(x, a, b):
+        return a * x + b
 
+    gamma=curve_fit(func, log_degree, log_prob)[0][0]
+    print('Degree exponent γ: ',gamma)
 
     num_components = nx.number_connected_components(graph)
     print("Number of connected components:", num_components)
 
+    largest_component = max(nx.connected_components(graph), key=len)
+    sub_graph = graph.subgraph(largest_component)
 
+    random_node1 = np.random.choice(list(sub_graph.nodes), size=10000, replace=True)
+    random_node2 = np.random.choice(list(sub_graph.nodes), size=10000, replace=True)
+    path_lengths = []
+    for i, j in zip(random_node1, random_node2):
+        if i!=j:
+            path_lengths.append(nx.shortest_path_length(sub_graph, i, j))
+
+    plt.hist(path_lengths, bins='auto')
+    plt.xlabel('Shortest Path Length')
+    plt.ylabel('Count')
+    plt.title('Shortest Path Length Distribution')
+    plt.savefig('shortestpathdist.pdf')
 
 
 if __name__ == '__main__':
     args = create_parser()
-    main()
+    main(args.input)
